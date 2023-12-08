46fa1f9e5a43a4f0d56a6dc521ab4f25f520bfcb
Nater88w <118359167+Nater88w@users.noreply.github.com>
2023-12-08 19:46:05 +0000

    final

--- src/getColor.js
+++ src/getColor.js
@@ -4,5 +4,7 @@ export default function getColor(){
         '#0072B5','#A0DAA9','#E9897E','#00A170','#926AA6','#D2386C'
     ];
     const ranNum = Math.floor(Math.random()*colors.length);
-    document.body.style.backgroundColor=colors[ranNum]
+    document.body.style.backgroundColor=colors[ranNum];
+    document.querySelector('#text').style.color=colors[ranNum];
+    document.querySelector('#author').style.color=colors[ranNum];
 }

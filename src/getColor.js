88a926370c9987b8f82d4fd0a5a83808acb1284e
Nater88w <118359167+Nater88w@users.noreply.github.com>
2023-12-08 10:00:56 +0000

    DSA

---
+++ src/getColor.js
@@ -0,0 +1,8 @@
+export default function getColor(){
+    const colors=[
+        '#34568B','#FF6F61','#6B5B95','#88B04B','#F7CAC9','#92A8D1','#FDAC53','#9BB7D4','#B55A30','#F5DF4D',
+        '#0072B5','#A0DAA9','#E9897E','#00A170','#926AA6','#D2386C'
+    ];
+    const ranNum = Math.floor(Math.random()*colors.length);
+    document.body.style.backgroundColor=colors[ranNum]
+}
